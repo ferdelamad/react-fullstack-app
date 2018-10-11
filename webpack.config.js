fa6881915ce7ef3c9f77dd482d1c4392ca3f743b
Fernando <ferdelamad@gmail.com>
2018-10-10 21:51:00 -0700

    React environment set up

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,30 @@
 const path = require("path");
 
 module.exports = {
-  entry: "./public/src/index.js",
+  entry: "./public/src/index.jsx",
   output: {
     filename: "main.js",
     path: path.resolve(__dirname, "./public/dist")
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          query: {
+            presets: ["@babel/preset-env", "@babel/preset-react"]
+          }
+        }
+      },
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"]
+      }
+    ]
+  },
+  resolve: {
+    extensions: [".js", ".jsx"]
   }
 };
