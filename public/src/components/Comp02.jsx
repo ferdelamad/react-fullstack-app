eb8d94d9c0765898d075f2c8979d5b3eb46e1720
Fernando <ferdelamad@gmail.com>
2018-10-17 23:46:16 -0700

    Initialized express server invironment.

--- public/src/components/Comp02.jsx
+++ public/src/components/Comp02.jsx
@@ -4,12 +4,12 @@ import { AppContext } from "./Provider";
 export const Comp02 = () => (
   <AppContext.Consumer>
     {context => (
-      <div>
+      <React.Fragment>
         <p>{context.state.name02}</p>
         <p>Test my increment function</p>
         <button onClick={context.incrementCounter}>Increment Counter</button>
         <p>Counter: {context.state.counter}</p>
-      </div>
+      </React.Fragment>
     )}
   </AppContext.Consumer>
 );
